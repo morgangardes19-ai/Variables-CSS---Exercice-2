87a88ba212acc55b5fb399d70261e1dd840d1558
morgan1000 <morgan.gardes.19@gmail.com>
2026-05-06 14:26:31 +0200

    avancée

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 let color = document.querySelector("#chooser");
+// let subtitleDiv = document.querySelector(".subtitle");
 
 color.addEventListener("input", handleClickOnInput);
+// subtitleDiv.addEventListener("input", handleClickOnInput);
 
 function handleClickOnInput() {
-  console.log(color.value);
-
+//   console.log(color.value);
   document.documentElement.style.setProperty("--main-color", color.value);
+//   subtitleDiv.style.setProperty("--main-color", color.value);
 }
